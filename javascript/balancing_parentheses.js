c746c9064f8cde536e56973e958777feb54cac68
Aruna <aruna.evan15@gmail.com>
2022-01-27 10:44:13 -0800

    Solution

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,9 +1,22 @@
 function balancingParentheses(string) {
-  // type your code here
+  let stack = [];
+  for(let i=0; i<string.length; i++){
+    if(stack.length===0 || string[i]==="("){
+      stack.push(string[i]);
+    }
+    else {
+      if(stack[stack.length-1]==="("){
+        stack.pop();
+      }
+      else {
+        stack.push(string[i]);
+      }
+    }
+  }
+  return stack.length;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: 0");
   console.log(balancingParentheses('(()())'));
 
@@ -16,9 +29,30 @@ if (require.main === module) {
 
   console.log("Expecting: 1");
   console.log(balancingParentheses(')'));
+
+  console.log("");
+
+  console.log("Expecting: 1");
+  console.log(balancingParentheses('('));
 }
 
 module.exports = balancingParentheses;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/**
+ * 1. paraphrase
+ * 
+ * return num of unclosed/unmatched parens in string of parens
+ * 
+ * 2. tests
+ * 
+ * '('
+ * 
+ * 3. pseudocode
+ * 
+ * create stack. pop off an open paren if followed by a close.
+ * return length of array at end.
+ * 
+ * 4. code
+ * 5. refactor
+ * 6. optimize if time
+ */
